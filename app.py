e06b37c0407a81cabcadad05a0e2045b8378a810
OierGman <75170798+OierGman@users.noreply.github.com>
2022-04-08 21:51:41 +0100

    adding the results page

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import json
-from flask import request
-from flask import Flask, render_template
+from flask import request, Flask, render_template, redirect, url_for
 from MathEquation import result_prediction
 
 app = Flask(__name__)
@@ -11,18 +10,28 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/test', methods=['POST'])
+@app.route('/test', methods=['POST', 'GET', 'PUT'])
 def test():
-    output = request.get_json()
-    result = json.loads(output)
-    print(result['place'])
-    print(result['type'])
-    print(result['room'])
-    room_int = int(result['room'])
-    print("............")
-    x = result_prediction(room_int, result['type'])
-    print(x)
-    return result
+    if request.method == "POST":
+        output = request.get_json()
+        result = json.loads(output)
+        print(result['place'])
+        print(result['type'])
+        print(result['room'])
+        room_int = int(result['room'])
+        prop_type = result['type']
+        print("............")
+        prediction = result_prediction(room_int, prop_type)
+        print(prediction)
+        return results()
+    else:
+        return render_template('index.html')
+
+
+@app.route('/results')
+def results():
+    return render_template('resultsPage.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
