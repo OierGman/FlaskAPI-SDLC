4c5643c9cf4f32cbf1e9246f64b61aa792e01c2f
OierGman <75170798+OierGman@users.noreply.github.com>
2022-04-16 23:02:19 +0100

    basic framework pulls and returns from template webpage.

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-import json
 from flask import request, Flask, render_template, redirect, url_for
 from MathEquation import result_prediction
 
@@ -10,27 +9,31 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/test', methods=['POST', 'GET', 'PUT'])
+@app.route('/', methods=['POST', 'GET'])
 def test():
     if request.method == "POST":
-        output = request.get_json()
-        result = json.loads(output)
-        print(result['place'])
-        print(result['type'])
-        print(result['room'])
-        room_int = int(result['room'])
-        prop_type = result['type']
-        print("............")
-        prediction = result_prediction(room_int, prop_type)
-        print(prediction)
-        return results()
+        # output = request.get_json()
+        # result = json.loads(output)
+        room_int = request.form["room"]
+        prop_type = request.form["type"]
+        location = request.form["place"]
+        bb = {
+            "location": location,
+            "type": prop_type,
+            "rooms": room_int
+        }
+        print(bb)
+        print("....................")
+        return redirect(url_for("results", location=location, rooms=room_int, p_type=prop_type))
     else:
         return render_template('index.html')
 
 
-@app.route('/results')
-def results():
-    return render_template('resultsPage.html')
+@app.route("/<location><rooms>bed<p_type>")
+def results(location, rooms, p_type):
+    prediction = result_prediction(int(rooms), p_type)
+    print(prediction)
+    return render_template('resultsPage.html', content=prediction)
 
 
 if __name__ == "__main__":
